4a40370bd7afa0d887b7dca847bdf98f56595a0e
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2025-12-10 16:24:00 +0700

    update ui v2

--- src/app/(client)/orders/page.tsx
+++ src/app/(client)/orders/page.tsx
@@ -261,7 +261,7 @@ export default function OrdersPage() {
                         key={order._id}
                         className="border border-gray-200 rounded-lg p-4 hover:shadow-md transition-shadow"
                       >
-                        <div className="flex justify-between items-start mb-3">
+                        <div className="flex justify-between flex-wrap gap-4 items-start mb-3">
                           <div>
                             <h3 className="text-base font-semibold text-gray-900">
                               Đơn hàng #{order._id.substring(0, 8).toUpperCase()}
